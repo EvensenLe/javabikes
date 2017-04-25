bec4fceae0681904f851381eddc150c10626ab61
Emilie Foss-Fjeldvik Evensen <emilie.foss.evensen@gmail.com>
2017-04-25 19:47:45 +0200

    Log in

--- EvensenLe/src/view/LogInView.java
+++ EvensenLe/src/view/LogInView.java
@@ -37,8 +37,11 @@ public class LogInView {
 				
 				//The do-while loop starts, everything in the curly-braces are executed in the loop. 
 				
-			System.out.println("To place the order, you need to login using the username and password created for you.");
-			System.out.println("You have 3 times try.");
+				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+				System.out.println("          PLEASE LOG IN IN ORDER TO MAKE AN ORDER          ");
+				System.out.println("                 You have three attempts.                  ");
+				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
 			
 			do{
 				counterTries++;
@@ -69,39 +72,11 @@ public class LogInView {
 				//We stay in the loop as long the counter for tries is less than the allowed number of tries
 				//and the user is not (!) logged-in.
 			}while(counterTries < numbersOfTries && !loggedIn);
-				System.out.println("You have exceeded the number of tries! Please try again in an hour.");
+				
 			
 			return loggedIn;
 			
-			//MAKE A LOOP FOR 3 TIMES TRY
-		//int tries = 0;
-			
-		//for (tries = 1; tries <=3; tries++) {
-					
-			//THE USE TYPES IN THEIR USERNAME AND PASSWORD
-		//System.out.println("Please type in your Username: ");
-		//String loginusername = input.next();
-						
-		//System.out.println("Please type in your Password: ");
-		//String loginpassword = input.next();
-					
-		
-		//HOW DO WE USE THE USERNAME AND PASSWORD FROM THE TXT FILE??
-		//if (MrCustomer.getUserName().equals(loginusername) && Customer.getPassword().equals(loginpassword)) {
-		//	System.out.println("You are now logged in!");
-		//}
-		//else if (tries >=3) {
-		//	System.out.println("You have exceeded the number of tries! Please try again in an hour.");
-		//}
-				
-		//else {
-		//	System.out.println("Your password and/or username is wrong. Please try again." );
-		//}
-	//}
-		
-		
-	
-//return customerHasToLogin();
+
 }
 	
 	
